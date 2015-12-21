19719f3b0991726b8e6ff47e4757f61776791cbd
henryleu <henryleu@126.com>
2015-12-21 15:55:07 +0800

    add ci branch

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var settings = module.exports = {
     id: 'agent-manager-No1',
     max_load: 40,
     api: {
-        url: 'http://dev.www.wenode.org/api'
+        url: 'http://ci.www.wenode.org/api'
     }
 };
